849d8f8dc9048ceac8803819b766cdbdea453896
ho3einkj <mkhalaj380@gmail.com>
2016-07-08 16:59:41 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '102214461:AAE47Y52C8MzW2pa1G0nkROzzYaOyL4SBbo');
+define('BOT_TOKEN', '233525147:AAGchkYq8CQ4E64Yh5watYFGWua1I0zdTI0');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
